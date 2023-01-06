bc1eccd9818b3e55167373e38e2825216d31e94e
anngofman <anngofman121@gmail.com>
2023-01-06 20:49:21 +0300

    Mogo end

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,22 @@
+$(document).ready(function () {
+  $('.header__burger').click(function (event) {
+    $('.header__burger, .header__body-menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+
+
+
 window.onscroll = function showHeader() {
   let header = document.querySelector('.header');
   let height = document.querySelector('.banner__background');
-  
+  let slid = document.querySelector('.slides');
+
   if (window.scrollY > (height.scrollHeight)) {
-    header.classList.add('header_fixed');
+    (header.classList.add('header_fixed'));
   } else {
     header.classList.remove('header_fixed');
   }
 }
 
-// тут мы вешаем на кнопку МЕНЮ событие которое при клике на кнопку
-// добавляет menu_mobile некий класс, в данном случае это active
 
-// document.querySelector('.btnmenu').addEventListener('click', () => {
-//   document.querySelector('.menu_mobile').classList.add('active');
-//   document.querySelector('.close-menu').classList.add('close-menu-active');
-// });
-// // а тут мы вешаем на кнопку close-menu событие которое пр кликена кнопку
-// // удаляет menu_mobile  класс  active
-
-// document.querySelector('.close-menu').addEventListener('click', () => {
-//   document.querySelector('.menu_mobile').classList.remove('active');
-//   document.querySelector('.close-menu').classList.remove('close-menu-active')
-// });
